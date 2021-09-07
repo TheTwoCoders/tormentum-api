e5ec6d0d184337eaf1adf8cfed0d1f11b6d00c06
Kersul <joao.faria@creditas.com>
2021-09-07 03:48:20 -0300

    chore: adding tslint

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Welcome to Tormentum API'));
 
 app.listen(PORT, () => {
+  // tslint:disable-next-line: no-console
   console.log(`⚡️[tormentum-api]: Server is running at http://localhost:${PORT}`);
 });
