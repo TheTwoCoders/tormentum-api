011ac4a0682ad1a814e043c0cbb54d74488aa585
Kersul <joao.faria@creditas.com>
2021-09-07 17:23:14 -0300

    feat: health routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import express from 'express';
+import { startServer } from './server'
 
-// rest of the code remains same
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => res.send('Welcome to Tormentum API'));
-
-app.listen(PORT, () => {
-  // tslint:disable-next-line: no-console
-  console.log(`⚡️[tormentum-api]: Server is running at http://localhost:${PORT}`);
-});
+startServer()
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import rootRoutes from './routes/root';
+import healthRoutes from './routes/health';
+
+const app = express();
+
+app.use('/', rootRoutes)
+app.use('/health', healthRoutes)
+
+const startServer = () => {
+  const PORT = process.env.PORT || 8080;
+
+  app.listen(PORT, () => {
+    // tslint:disable-next-line: no-console
+    console.log(`⚡️[tormentum-api]: Server is running at http://localhost:${PORT}`);
+  });
+}
+
+export { startServer }
