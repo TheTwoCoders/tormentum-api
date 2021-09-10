a2a5bfcef3c488062a3733338fcf766fc34fb1dc
Otavio Augusto Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2021-09-09 21:03:32 -0300

    refactor: deleteUser now returns null instead of returning a string

--- src/use_cases/deleteUser.ts
+++ src/use_cases/deleteUser.ts
@@ -2,14 +2,13 @@ import { ObjectId } from 'mongoose'
 import UserNotFound from '../exceptions/UserNotFound'
 import UserModel, { User } from '../models/UserModel'
 
-const deleteAccount = async (_id: ObjectId): Promise<string> => {
+const deleteAccount = async (_id: ObjectId): Promise<void> => {
     const user = await UserModel.findOne({ _id })
     if (user == null) {
         throw new UserNotFound(`User not found for Id ${_id}`)
     }
 
     await user.deleteOne()
-    return "ACCOUNT_DELETED"
 }
 
 export default deleteAccount
