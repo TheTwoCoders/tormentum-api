24c124e6f6b5d66217def987d9db7e25bebb8c0e
Otavio Augusto Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2021-09-10 22:12:18 -0300

    fix:updateUser does not generate any new warnings anymore

--- src/use_cases/updateUser.ts
+++ src/use_cases/updateUser.ts
@@ -1,8 +1,9 @@
 import { ObjectId } from "mongoose"
 import UserNotFound from "../exceptions/UserNotFound"
 import { findUserById, updateUserById } from "../repositories/UserRepository"
+import User from "../domain/User"
 
-const updateUser = async (id: ObjectId, content: { username?: string, email?: string, password?: string }): Promise<User> => {
+const updateUser = async (id: ObjectId, content: { username?: string, email?: string, password?: string }): Promise<User | null> => {
   const user = await findUserById(id)
   if (user == null) {
     throw new UserNotFound(`User not found for Id ${id}`)
